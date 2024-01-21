fa0b1661f93bd780c70f11fa4907aef6ff6a963c
mahmudul <imdmahmudul237@gmail.com>
2024-01-21 18:48:52 +0600

    fix cors policy

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const client = require('./connect-mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true, 
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
